9ee7ebe9a7f3b97a11bc5a55e6971f74ce1e366d
César González <bolobox@gmail.com>
2018-04-18 14:06:30 +0200

    Some cosmetic changes in telegram messages

--- app/strategies/ichimokub.py
+++ app/strategies/ichimokub.py
@@ -16,13 +16,11 @@ class IchimokuStrategyB(BaseStrategy):
     def prenext(self):
         self.log("prenext", datetime.utcnow())
 
-
     def __init__(self):
         self.inds = dict()
         for i, d in enumerate(self.datas):
             self.inds[d] = dict()
             self.inds[d]['ichimoku'] = bt.indicators.Ichimoku(d, plot=True)
-
 
     def next(self):
         self.log("next called", datetime.utcnow())
@@ -33,14 +31,14 @@ class IchimokuStrategyB(BaseStrategy):
             if len(data0.close)>1 and len(ichimoku.senkou_span_a) > 1 and len(ichimoku.senkou_span_b) > 1 :
                 if (data0.close[0] > max([ichimoku.senkou_span_a[0], ichimoku.senkou_span_b[0]])
                     and data0.close[-1] < max([ichimoku.senkou_span_a[-1], ichimoku.senkou_span_b[-1]])):
-                    msg = "*Buy* signal on %s" % (name)
+                    msg = "**Buy** signal on %s" % (name)
                     self.log(msg)
                     self.notify_operator(msg)
                     self.log('------ %s PRICE %.6f - timestamp %s' % (name, data0.close[0], bt.num2date(data0.datetime[0]).isoformat()), datetime.utcnow())
 
                 if (data0.close[0] < min([ichimoku.senkou_span_a[0], ichimoku.senkou_span_b[0]])
                     and data0.close[-1] > min([ichimoku.senkou_span_a[-1], ichimoku.senkou_span_b[-1]])):
-                    msg = "*Sell* signal on %s" % (name)
+                    msg = "**Sell** signal on %s" % (name)
                     self.log(msg)
                     self.notify_operator(msg)
                     self.log('------ %s PRICE %.6f - timestamp %s' % (name, data0.close[0], bt.num2date(data0.datetime[0]).isoformat()), datetime.utcnow())
